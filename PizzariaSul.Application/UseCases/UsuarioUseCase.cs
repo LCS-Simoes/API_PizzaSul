a4992c671cb562dcd529b9f44990bae3d22fba28
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-27 21:58:37 -0300

    feat(Usuarios): CRUD

--- PizzariaSul.Application/UseCases/UsuarioUseCase.cs
+++ PizzariaSul.Application/UseCases/UsuarioUseCase.cs
@@ -1,11 +1,11 @@
 ﻿using PizzariaSul.Application.DTOs.UsuariosDTOs;
-using PizzariaSul.Application.Interfaces;
+using PizzariaSul.Domain.Entidades;
 using PizzariaSul.Domain.Interfaces;
 
 
 namespace PizzariaSul.Application.UseCases
 {
-    public class UsuarioUseCase : IUsuarioApplication
+    public class UsuarioUseCase 
     {
         private readonly IUsuarios _usuarioReposotiro;
 
@@ -14,19 +14,77 @@ namespace PizzariaSul.Application.UseCases
             _usuarioReposotiro = usuarioReposotiro;
         }
 
-        public Task<UsuarioResponse> BuscarId(int id)
+        public async Task<UsuarioResponse> ExecuteAsync(CadastrarRequest request) //Pode dar erro verificar posteriormente
         {
-            throw new NotImplementedException();
+            var usuario = new Usuario
+            {
+                Nome = request.Nome,
+                Celular = request.Celular,
+                Bairro = request.Bairro,
+                Rua = request.Rua,
+                NumeroCasa = request.NumeroCasa,
+                Cidade = request.Cidade,
+                Cep = request.Cep
+            };
+
+            var criado = await _usuarioReposotiro.CadastrarUsuario(usuario);
+
+            return new UsuarioResponse
+            {
+                Id = criado.Id,
+                Nome = criado.Nome,
+                Celular = criado.Celular,
+                Bairro = criado.Bairro,
+                Rua = criado.Rua,
+                NumeroCasa = criado.NumeroCasa,
+                Cidade = criado.Cidade,
+                Cep = criado.Cep
+            };
         }
 
-        public Task<UsuarioResponse> Deletar(int id)
+        public async Task<UsuarioResponse> AtualizarUsuario(int id, AtualizarRequest request)
         {
-            throw new NotImplementedException();
+            var usuario = await _usuarioReposotiro.BuscarId(id);
+            if (usuario == null) return null;
+
+            var atualizado = await _usuarioReposotiro.AtualizarUsuario(usuario, id);
+
+            return new UsuarioResponse
+            {
+                Id = atualizado.Id,
+                Nome = atualizado.Nome,
+                Celular = atualizado.Celular,
+                Bairro = atualizado.Bairro,
+                Rua = atualizado.Rua,
+                NumeroCasa = atualizado.NumeroCasa,
+                Cidade = atualizado.Cidade,
+                Cep = atualizado.Cep
+            };
         }
 
-        public Task<UsuarioResponse> ExecuteAsync(CadastrarRequest request)
+        //Remover pós testes
+        public async Task<UsuarioResponse> BuscarId(int id)
         {
-            throw new NotImplementedException();
+            var usuario = await _usuarioReposotiro.BuscarId(id);
+            if (usuario == null) return null;
+
+            return new UsuarioResponse
+            {
+                Id = usuario.Id,
+                Nome = usuario.Nome,
+                Celular = usuario.Celular,
+                Bairro = usuario.Bairro,
+                Rua = usuario.Rua,
+                NumeroCasa = usuario.NumeroCasa,
+                Cidade = usuario.Cidade,
+                Cep = usuario.Cep
+            };
+            
+        }
+
+        public async Task<bool> Deletar(int id)
+        {
+            return await _usuarioReposotiro.DeletarUsuario(id);
         }
     }
 }
