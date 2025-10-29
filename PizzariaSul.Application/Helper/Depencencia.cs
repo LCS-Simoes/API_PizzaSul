293a07768643df4904f4bc2981f298ce887d2934
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-28 23:27:53 -0300

    Dependencia usuario

---
+++ PizzariaSul.Application/Helper/Depencencia.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PizzariaSul.Application.UseCases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzariaSul.Application.Helper
+{
+    public static class Depencencia
+    {
+        public static IServiceCollection AddUseCases(this IServiceCollection services)
+        {
+            services.AddScoped<UsuarioUseCase>();
+            return services;
+        }
+    }
+}
